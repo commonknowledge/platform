2e981e3127dd9c5f7e3c11f4eb9a106863422f77
Joaquim d'Souza <joaquimds@gmail.com>
2023-09-07 16:49:30 +0100

    feat: add statements to debug slow wordpress

--- web/index.php
+++ web/index.php
@@ -3,4 +3,19 @@
  * WordPress View Bootstrapper
  */
 define('WP_USE_THEMES', true);
+
+define('START_TIME', microtime(true));
+
+function print_execution_time($tag)
+{
+    $time = microtime(true) - START_TIME;
+    $seconds = $time / 1000;
+    $str = number_format($seconds, 3);
+    echo "<!-- $tag: {$str}s -->";
+}
+
+print_execution_time("Start");
+
 require __DIR__ . '/wp/wp-blog-header.php';
+
+print_execution_time("Done");
