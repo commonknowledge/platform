ec7407e00200960185d829587189284967f1b2b6
Joaquim d'Souza <joaquimds@gmail.com>
2023-09-11 16:48:30 +0200

    test: try setting content-length header to fix nginx slowness

--- web/test.php
+++ web/test.php
@@ -1,6 +1,15 @@
 <?php
 
+ob_start();
+
 $start = $_SERVER['HTTP_X_START_TIME'];
 $end = microtime(true);
 
-echo "Request took " . round(($end - $start) * 1000, 2) . " milliseconds.";
+echo "Request took " . round(($end - $start) * 1000, 2) . " milliseconds.\n";
+
+$content = ob_get_clean();
+$length = strlen($content);
+
+header('Content-Length: '.$length);
+
+echo $content;
