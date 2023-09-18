6d509444541c4ef1f6ca0bb6e4ecd94448f7d71b
Joaquim d'Souza <joaquimds@gmail.com>
2023-09-18 19:17:30 +0200

    feat: put back fix_small_images command

--- web/app/themes/platformlondon/src/commands.php
+++ web/app/themes/platformlondon/src/commands.php
@@ -40,3 +40,51 @@ function merge_categories($args)
     }
 }
 \WP_CLI::add_command('merge_categories', 'merge_categories');
+
+function find_largest_version($filename, $dir)
+{
+    $largest_filename = null;
+    $largest_width = 0;
+    $filenames = scandir($dir);
+    foreach ($filenames as $file) {
+        $original_filename = preg_replace("#-[0-9]{3,4}x[0-9]{3,4}#", "", $file);
+        if ($original_filename === $filename) {
+            preg_match("#-([0-9]{3,4}x[0-9]{3,4})#", $file, $matches);
+            if (count($matches) > 1) {
+                $dimensions = $matches[1];
+                list($width, $height) = explode("x", $dimensions);
+                if ($width > $largest_width) {
+                    $largest_filename = $file;
+                }
+            }
+        }
+    }
+    return $largest_filename;
+}
+
+function fix_small_images($args)
+{
+    $uploads_path = __DIR__ . "/../../../uploads";
+    $years = scandir($uploads_path);
+    foreach ($years as $year) {
+        $months = scandir($uploads_path . "/" . $year);
+        foreach ($months as $month) {
+            $dir = $uploads_path . "/" . $year . "/" . $month;
+            $filenames = scandir($dir);
+            foreach ($filenames as $filename) {
+                $largest_version = find_largest_version($filename, $dir);
+                $largest_filepath = $dir . "/" . $largest_version;
+                $filepath = $dir . '/' . $filename;
+                if ($largest_version) {
+                    list($width, $height, $type, $attr) = getimagesize($filepath);
+                    list($l_width, $height, $type, $attr) = getimagesize($largest_filepath);
+                    if ($l_width > $width) {
+                        echo "REPLACING " . $filename . " WITH " . $largest_version . "\n";
+                        copy($largest_filepath, $filepath);
+                    }
+                }
+            }
+        }
+    }
+}
+\WP_CLI::add_command('fix_small_images', 'fix_small_images');
