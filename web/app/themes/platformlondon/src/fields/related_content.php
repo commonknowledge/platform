b39b1ceb7a663a83667623ea2bfacec8982b12b6
Joaquim d'Souza <joaquimds@gmail.com>
2023-09-25 12:47:35 +0200

    feat: improve related block - add multimedia and improve search

--- web/app/themes/platformlondon/src/fields/related_content.php
+++ web/app/themes/platformlondon/src/fields/related_content.php
@@ -26,3 +26,31 @@ Container::make('post_meta', 'Related Content')
                 ]
             ])
     ));
+
+// Restrict related posts search to title only
+// https://github.com/htmlburger/carbon-fields/issues/957
+function pl_search_by_title($options)
+{
+    $options['search_by_title'] = $options['s'] ?? "";
+    unset($options['s']);
+    return $options;
+}
+
+add_filter("carbon_fields_association_field_options_related_post_pl_resource", 'pl_search_by_title');
+
+add_filter("carbon_fields_association_field_options_related_post_post", 'pl_search_by_title');
+
+add_filter("carbon_fields_association_field_options_related_post_pl_project", 'pl_search_by_title');
+
+add_filter("carbon_fields_association_field_options_related_post_pl_multimedia", 'pl_search_by_title');
+
+add_filter('posts_where', function ($where, $wp_query) {
+    global $wpdb;
+
+    $search_by_title = $wp_query->get('search_by_title');
+    if (!empty($search_by_title)) {
+        $where .= ' AND ' . $wpdb->posts . '.post_title LIKE \'%' . esc_sql($wpdb->esc_like($search_by_title)) . '%\'';
+    }
+
+    return $where;
+}, 10, 2);
