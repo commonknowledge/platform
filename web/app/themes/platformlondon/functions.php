5cf3939d47dc13275c9b2673d59fe2f90ce0b7c6
Alex Worrad-Andrews <alex@alexandrews.info>
2024-05-15 11:10:27 +0100

    Mark as important to force the change

--- web/app/themes/platformlondon/functions.php
+++ web/app/themes/platformlondon/functions.php
@@ -106,7 +106,7 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style(
         'platformlondon',
         get_template_directory_uri() . '/style.css',
-        ver: "1.14",
+        ver: "1.15",
     );
     wp_enqueue_script(
         'platformlondon-pre',
